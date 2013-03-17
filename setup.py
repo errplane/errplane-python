92733099270a5fbf065efe15c2a3bab703fde64a
Todd Persen <todd.persen@gmail.com>
2013-03-17 01:05:46 -0700

    Set up basic package structure.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='errplane',
+    version='0.0.1',
+    author='Todd Persen',
+    author_email='todd@errplane.com',
+    packages=find_packages(),
+    url='http://github.com/errplane/errplane-python',
+    license='LICENSE.txt',
+    description='Python library for use with Errplane (https://errplane.com)',
+    install_requires=[
+    ],
+)
